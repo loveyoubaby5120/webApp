6177eea770afad12a8b017b7c86c304cd7cb288f
pankai <273377900@qq.com>
2016-03-31 17:11:11 +0800

    gongodb

---
+++ condition.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+require('./model.js');
+
+var Book = mongoose.model('Book');
+
+var  cond_or = {
+	$or: [
+		{author:'pankai'},
+		{author:'kai'}
+	]
+};
+
+var  cond_add = {
+	$add: [
+		{author:'pankai'},
+		{author:'kai'}
+	]
+};
+
+Book.find(cond_or,function(err,docs){
+	if(err){
+		console.log('err: ',err);
+		return;
+	}
+
+	console.log('cond: ',cond,'result: ',docs);
+
+});
