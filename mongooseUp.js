8d8781b48d4621046ad3e29b9cb48e6b4a39ea0a
pankai <273377900@qq.com>
2016-04-01 15:33:54 +0800

    mongooseUp

--- mongooseUp.js
+++ mongooseUp.js
@@ -2,41 +2,63 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/webApp');
 
-var BookSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		default: 'pankai',
-		get: function(name){
-			if(!name) return name;
-			if(0 !== name.indexOf('pk_') && 0!== name.indexOf('pk_'));
-				name = 'pk_' +  name;
 
-			return name;
+var UserSchema = mongoose.Schema({
+	username: String
+});
+
+var BookSchema = new mongoose.Schema({
+title: {
+		type: String,
+		enum: ['pankai','system'],
+		default: 'pankai',
+		trim: true,
+		get: function(title){
+			if(!title) return title;
+			if(0 !== title.indexOf('pk_') && 0!== title.indexOf('pk_'));
+				title = 'pk_' +  title;
+
+			return title;
 		}
+	},
+	author: {
+		type: mongoose.Schema.ObjectId,
+		ref:'UserSchema'
 	},
 	isbn: {
 		type: Number,
-		trim: true
-	},
-	createTime: {		
-		type: Date,
-		default: Date.now
+		default: 1
 	},
 	blog: {
 		type: String,
+		match: /love/g,
+		validate: function(blog){
+			return blog.length >= 20;
+		},
+		default: 'http://wwww.loveyoubaby5120.com',
 		set: function(url){
 			if(!url) return url;
 			if(0 !== url.indexOf('http://') && 0!== url.indexOf('https://'))
 				url = 'http://' +  url;
-
 			return url;
 		}
+	},
+	count: {
+		type: Number,
+		required: true,
+		max: 1000,
+		min: 10,
+		default: 10
+	},
+	createTime: {
+		type: Date,
+		default: Date.now
 	}
 });
 
 
 BookSchema.virtual('userBlog').get(function(){
-	return this.username + ' is blog url ' + this.blog;
+	return this.title + ' is blog url ' + this.blog;
 });
 
 BookSchema.set('toJSON',{getters:true,virtual:true});
@@ -51,41 +73,89 @@ BookSchema.statics.findByISBN = function(isbn,cb){
 
 BookSchema.methods.print = function(){
 	console.log('Book information');
-	console.log('\tusername: ',this.username);
+	console.log('\ttitle: ',this.title);
 	console.log('\tisbn: ',this.isbn);
 }
 
 
+BookSchema.post('save',function(next){
+	console.log('this is BookSchema post save middleware');
+	// next();   ?????????????
+});
+
+
+BookSchema.pre('save',true,function(next,done){
+	console.log('this is BookSchema pre save middleware');
+	next();
+	done();
+});
+
+
+var User = mongoose.model('UserSchema',UserSchema);
+
+
 var Book = mongoose.model('BookSchema',BookSchema);
+
+var user = new User({
+	username: 'pankai'
+});
 
 
 var book = new Book({
-	username:  'system',
-	isbn: 1,
-	blog: 'loveyoubaby5120.com'
+	title:  'system',
+	author: user
+	// isbn: 1,
+	// blog: 'loveyoubaby5120.com',
 });
 
-
-// console.log('book: ',book);
-// console.log('book.username: ',book.username);
-// console.log('book JSON: ',JSON.stringify(book));
-
-
-book.save(function(err,doc){
+user.save(function(err){
 	if(err){
-		return console.log('save book failed',err);
+		return console.log('save user failed: ',err);
 	}
 
 
-	Book.findByISBN(1,function(err,doc){
-		console.log('findByIsNB, err, doc', err, doc);
-	});
+	book.save(function(err,doc){
+		if(err){
+			return console.log('save book failed',err);
+		}
 
-	book.print();
+		Book.findOne().populate('author').exec(function(err,doc){
+			console.log('after populate: ', err, doc);
+		});
+
+	});
 
 });
 
+// console.log('book: ',book);
+// console.log('book.title: ',book.title);
+// console.log('book JSON: ',JSON.stringify(book));
+
+// book.save(function(err,doc){
+// 		if(err){
+// 			return console.log('save book failed',err);
+// 		}
+
+
+// 		Book.findByISBN(1,function(err,doc){
+// 			console.log('findByIsNB, err, doc', err, doc);
+// 		});
+
+// 		book.print();
+
+// });		
 
 
 
+// var book2 = new Book({
+// 	title:  'git',
+// 	isbn: 1,
+// 	blog: 'loginUp',
+// 	count: 1111111
+// });
 
+// book2.save(function(err){
+// 	if(err){
+// 		return console.log('err: ',err);
+// 	}
+// });
