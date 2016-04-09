c7de7a8a0ffc9b9e5cf5c65338cb839d84efa55d
pankai <loveyoubaby5120@sina.com>
2016-04-09 10:51:13 +0800

    other

--- ExpressMongoose/app.js
+++ ExpressMongoose/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser = requ ire('body-parser');
 
 var mongoose = require('./config/mongoose.js');
 
