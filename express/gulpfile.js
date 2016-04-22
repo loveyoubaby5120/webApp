aa6f5a9c5e11d4b6eca83372b8ecba32862bb011
pankai <loveyoubaby5120@sina.com>
2016-04-22 13:46:02 +0800

    webpack

--- express/gulpfile.js
+++ express/gulpfile.js
@@ -1,20 +1,117 @@
 var gulp = require('gulp');
-var react = require('gulp-react');  //沾化jsx代码
+//转换jsx代码
+var react = require('gulp-react');
 var babel = require('gulp-babel');
+
+
+//转换less
 var less = require('gulp-less');
+//转换sass
 var sass = require('gulp-sass');
+
+//压缩css  npm install gulp-minify-css --save-dev
+var minifyCss = require('gulp-minify-css');
+
+//压缩js
 var uglify = require('gulp-uglify');
 
+
+//打包js
 var browserrify = require('gulp-browserify');
-var source = require('vinyl-source-stream');   //把browserify  输出转化为gulp 可识别代码
-var reactify = require('reactify');            //把browserify  输出jsx转化为 转化js代码
+//把browserify  输出转化为gulp 可识别代码
+var source = require('vinyl-source-stream');
+//把browserify  输出jsx转化为 转化js代码
+var reactify = require('reactify');
 
-var webpack = require("webpack");			//加载webpack
-var webpackConfig = require("./webpack.config.js");   //加载webpack配置
+//加载webpack
+var webpack = require("webpack");
+//加载webpack配置
+var webpackConfig = require("./webpack.config.js");
 
-var browserSync = require("browser-sync"); //自动刷新浏览器   npm install browser-sync --save-dev
+//自动刷新浏览器   npm install browser-sync --save-dev
+var browserSync = require("browser-sync");
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+//控制插件执行顺序
+var runSequence = require('gulp-run-sequence');
+
+//自动执行node  $ npm install --save-dev gulp-nodemon
+var nodemon = require('gulp-nodemon ');
+
+//合并js
+var concat = require('gulp-concat');
+ 
+
+
+var paths = {
+  main: './client.js',
+  jsx: './components/**/**.jsx',
+  stylusMain: './components/app.styl',
+  stylusAll: './components/**/*.styl',
+  css: './public/css/',
+  server: './server.js',
+  serverIgnore: [
+    'gulpfile.js',
+    'public/',
+    'components/**/*.styl',
+    'bower_components/',
+    'node_modules/'
+  ],
+  publicJs: './public/js'
+};
+
+gulp.task('scripts', function() {
+  // gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js']
+
+  return gulp.src('./lib/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+
+
+var reloadDelay = 6500;
+
+gulp.task('server', function(cb) {
+  var called = false;
+  nodemon({
+    script: paths.server,
+    ext: '.js .css',
+    ignore: paths.serverIgnore,
+    env: {
+      'NODE_ENV': 'development',
+      'DEBUG': 'r3dm:*'
+    }
+  })
+    .on('start', function() {
+      if (!called) {
+        called = true;
+        setTimeout(function() {
+          cb();
+        }, reloadDelay);
+      }
+    })
+    .on('restart', function(files) {
+      if (files) {
+        debug('Files that changed: ', files);
+      }
+      setTimeout(function() {
+        debug('Restarting browsers');
+        reload();
+      }, reloadDelay);
+    });
+});
+
+
+/*设置插件执行顺序-------------------------------------------------------------------------------------------------------------------------*/
+
+gulp.task('prod', function(cb) {  
+    //圆括号里面串行，中括号里面并行
+    runSequence('clean', 'compass', ['image', 'style', 'html'], 'ftp', cb);
+});
+
+
+
+/*设置浏览器自动刷新---------------------------------------------------------------------------------------------------------------------*/
 
 
 // 自动刷新 browser-sync start
@@ -54,12 +151,9 @@ gulp.task('default',['bro','browser'],function(){
 });
 
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
 
 
-
-
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*执行webpack-------------------------------------------------------------------------------------------------------------------------*/
 
 gulp.task("webpack", function(callback) {
   var myConfig = Object.create(webpackConfig);
@@ -76,12 +170,13 @@ gulp.task("webpack", function(callback) {
   });
 });
 
-gulp.watch('app/src/**/*.js', ['webpack']); //监控js变化重新webpack
+//监控js变化重新webpack
+gulp.watch('app/src/**/*.js', ['webpack']);
 
 
 
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*转换jsx代码-------------------------------------------------------------------------------------------------------------------*/
 
 gulp.task('jsx', function() {
 	browserify({
@@ -97,30 +192,48 @@ gulp.task('jsx', function() {
 gulp.task('jsx', function() {
 
     gulp.src('./src/index.js')
-        .pipe(browserify())       //打包js
-        .pipe(uglify())				//压缩js
+        //打包js
+        .pipe(browserify())
+        //压缩js
+        .pipe(uglify())
         .pipe(gulp.dest('./dest'));
 }
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*压缩js-------------------------------------------------------------------------------------------------------------------------------*/
 
 gulp.task('jsmin', function () {
     gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
         .pipe(uglify({
-            // mangle: true,//类型：Boolean 默认：true 是否修改变量名
-            mangle: {except: ['require' ,'exports' ,'module' ,'$']}//排除混淆关键字
-            compress: true,//类型：Boolean 默认：true 是否完全压缩
-            preserveComments: all //保留所有注释
+            //类型：Boolean 默认：true 是否修改变量名
+            // mangle: true,
+            //排除混淆关键字
+            mangle: {except: ['require' ,'exports' ,'module' ,'$']},
+            //类型：Boolean 默认：true 是否完全压缩
+            compress: true,
+            //保留所有注释
+            preserveComments: all
         }))
         .pipe(gulp.dest('dist/js'));
 });
 
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*压缩css-------------------------------------------------------------------------------------------------------------------------*/
+
+gulp.task('testCssmin', function () {
+    gulp.src('src/css/*.css')
+        .pipe(cssmin({
+            //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            advanced: false,
+            //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            compatibility: 'ie7',
+            //类型：Boolean 默认：false [是否保留换行]
+            keepBreaks: true
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
 
 
-
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*转换less sass-----------------------------------------------------------------------------------------------------------------------------*/
 
 
 gulp.task('less',function(){
@@ -135,7 +248,7 @@ gulp.task('sass',function(){
 			.pipe(gulp.dest('./public/dest'));
 });
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*默认执行插件------------------------------------------------------------------------------------------------------------------------------*/
 
 gulp.task('default',['less','sass'],function(){
 	return gulp.src('./public/js/*.js')
@@ -145,3 +258,17 @@ gulp.task('default',['less','sass'],function(){
 				}))
 				.pipe(gulp.dest('./public/dest'));
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- express/webpack.config.js
+++ express/webpack.config.js
@@ -1,26 +1,153 @@
+// 把指定文件夹下的文件复制到指定的目录 npm install transfer-webpack-plugin  -save
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
 var webpack = requier('webpack');
+//压缩js插件
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+//提取共同的代码
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+//允许错误不打断程序
+var NoErrorsPlugin = webpack.NoErrorsPlugin;
+
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
 	// entry: './public/js/index.js', //接入点
 	entry:{
 		app:'./public/js/index.js',
-		app2:'./public/js/about.js'
-	}
+		app2:'./public/js/about.js',
+		//【1】注意这里
+		common: ["./public/dest/common"]
+	},
+	//用于指明程序自动补全识别哪些后缀
+	resolve: {
+	    extensions: ['', '.coffee', '.js']
+	},
+    //Server Configuration options
+    devServer:{
+        contentBase: '',  //静态资源的目录 相对路径,相对于当前路径 默认为当前config所在的目录
+        devtool: 'eval',
+        hot: true,        //自动刷新
+        inline: true,    
+        port: 3005        
+    },
+    devtool: 'eval',
 	output: {
-		paath: './public/dest',      //输出目录
-		// finlename: 'app.js',  //输出文件名
-		finlename: '[name].js',  //输出文件名
-		publicPath: "./server/",				//html引用路径，在这里是本地地址。
-	}，
+		//输出目录
+		paath: './public/dest',
+		//输出文件名
+		// finlename: 'app.js',
+		// //输出文件名
+		finlename: '[name].js',
+		//html引用路径，在这里是本地地址。
+		publicPath: "./server/",
+	},
 	plugins:[
-		new webpack.optimize.CommonsChunkPlugin("common.js")   //提取共同的代码
-	]
+		//提取共同的代码
+		//【2】注意这里  这两个地方市用来配置common.js模块单独打包的
+		new CommonsChunkPlugin(
+			{
+				//和上面配置的入口对应
+	            name: "common",
+	            //导出的文件的名称
+	            filename: "common.js"
+        	}
+        ),
+		//压缩js
+		new uglifyJsPlugin({
+	      compress: {
+	        warnings: false
+	      },
+	      //排除混淆关键字
+	      mangle: {
+	      	except: ['require' ,'exports' ,'module' ,'$']
+	      }
+	    }),
+	    //允许错误不打断程序
+    	new NoErrorsPlugin(),
+    	//把指定文件夹xia的文件复制到指定的目录
+	    new TransferWebpackPlugin(
+	    	[
+	      		{from: 'www'}
+	    	], 
+	    	path.resolve(__dirname,"src")
+	    )
+	],
 	module: {
+		preLoaders: [
+		    {
+		      test: /\.(js|jsx)$/,   //注意是正则表达式，不要加引号，匹配要处理的文件
+		      loader: 'eslint-loader',  //要使用的loader，"-loader"可以省略
+		      include: [path.resolve(__dirname, "src/app")],  //把要处理的目录包括进来
+		      exclude: [nodeModulesPath]  //排除不处理的目录
+		    },
+	    ],
 		loders: [
+		    {
+		    	////打包静态资源
+		    	test: /\.(js|jsx)$/, //正则表达式匹配 .js 和 .jsx 文件  ç
+		    	loader: 'babel-loader?optional=runtime&stage=0',//对匹配的文件进行处理的loader 
+		    	exclude: [nodeModulesPath]//排除node module中的文件
+		    }
 			{
 				test: /\.jsx$/, //匹配文件
 				loader: 'jsx-loder', // jsx转化为js    npm install jsx-loder
-			}
+        		exclude: [nodeModulesPath]//排除node module中的文件
+			},
+			{
+				// use ! to chain loaders
+				test: /\.less$/, 
+				loader: 'style-loader!css-loader!less-loader',  //npm install css-loader --save -dev npm install style-loader --save -dev
+        		exclude: [nodeModulesPath]//排除node module中的文件 
+			}, 
+		    {   
+		    	//打包静态资源
+		    	test: /\.css$/,
+		    	loader: 'style-loader!css-loader' ,  //npm install css-loader --save -dev npm install style-loader --save -dev
+        		exclude: [nodeModulesPath]//排除node module中的文件 
+		    },
+		    {   
+		    	// 可以通过url-loader把较小的图片转换成base64的字符串内嵌在生成的文件里。 npm install url-loader --save -dev
+		    	test: /\.(png|jpg)$/,
+		    	loader: 'url-loader?limit=8192',    //   query: { mimetype: "image/png" }  //限制大小小于8.192k的
+        		exclude: [nodeModulesPath]//排除node module中的文件 
+		    },
+		    {
+		    	test:/\.(png|woff|svg|ttf|eot)$/,
+		    	loader:'url-loader?limit=10000',//限制大小小于10k的
+        		exclude: [nodeModulesPath]//排除node module中的文件 
+	    	},
+	    	{   
+	    		// 打包template   npm install handlebars-loader --save -dev  npm install handlebars -save//是必须的
+	    		test: /\.html$/,
+	    		loader: "handlebars-loader",
+        		exclude: [nodeModulesPath]//排除node module中的文件 
+	    	},
+
 		]
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
