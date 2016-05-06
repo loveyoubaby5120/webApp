2c68072db40bd51f3dc3ea708e991739e604b8e8
pankai <loveyoubaby5120@sina.com>
2016-05-06 16:52:23 +0800

    log

--- spider/routes/excel.js
+++ spider/routes/excel.js
@@ -4,6 +4,7 @@ var formidable = require('formidable');
 var fs = require('fs');
 var xlsx = require('node-xlsx');
 var path = require('path');
+//npm install pyspreadsheet --save-dev
 // var SpreadsheetReader = require('pyspreadsheet').SpreadsheetReader;
 var excelPort = require('excel-export');
 
@@ -17,62 +18,70 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
 
-    var datas = [['a','b','c','d','e','f','g','h','i'],['a2','b2','c2','d2','e2','f2','g2','h2','i2']];
-    var conf = {};
-    var filename = 'filename';  //只支持字母和数字命名
+    // var datas = [['a','b','c','d','e','f','g','h','i'],['a2','b2','c2','d2','e2','f2','g2','h2','i2']];
+    // var conf = {};
+    // var filename = 'filename';  //只支持字母和数字命名
     
-    conf.cols = [
-       {caption:'名称', type:'string', width:20},
-       {caption:'简介', type:'string', width:40},
-       {caption:'报酬', type:'string', width:20},
-       {caption:'时间', type:'date', width:40},
-       {caption:'人员', type:'string', width:30},
-       {caption:'编号', type:'string', width:30},
-       {caption:'金额', type:'number', width:30},
-       {caption:'手机号', type:'string', width:30}
-    ];
+    // conf.cols = [
+    //    {caption:'名称', type:'string', width:20},
+    //    {caption:'简介', type:'string', width:40},
+    //    {caption:'报酬', type:'string', width:20},
+    //    {caption:'时间', type:'date', width:40},
+    //    {caption:'人员', type:'string', width:30},
+    //    {caption:'编号', type:'string', width:30},
+    //    {caption:'金额', type:'number', width:30},
+    //    {caption:'手机号', type:'string', width:30}
+    // ];
 	
-    var array = [];
+    // var array = [];
     
-    array[0] = [
-        datas[0][0],
-          datas[0][1],
-          datas[0][2],
-          datas[0][3],
-          datas[0][4],
-          datas[0][5],
-          datas[0][6],
-          datas[0][7]
-    ];
+    // array[0] = [
+    //     datas[0][0],
+    //       datas[0][1],
+    //       datas[0][2],
+    //       datas[0][3],
+    //       datas[0][4],
+    //       datas[0][5],
+    //       datas[0][6],
+    //       datas[0][7]
+    // ];
 
-     array[1] = [
-        datas[1][0],
-          datas[1][1],
-          datas[1][2],
-          datas[1][3],
-          datas[1][4],
-          datas[1][5],
-          datas[1][6],
-          datas[1][7]
-    ];
-
-
-    conf.rows = array;
-    var result = excelPort.execute(conf);
+    //  array[1] = [
+    //     datas[1][0],
+    //       datas[1][1],
+    //       datas[1][2],
+    //       datas[1][3],
+    //       datas[1][4],
+    //       datas[1][5],
+    //       datas[1][6],
+    //       datas[1][7]
+    // ];
 
 
-    var random = Math.floor(Math.random()*10000+0);
+    // conf.rows = array;
+    // var result = excelPort.execute(conf);
 
-    var uploadDir = 'public/upload/';
-    var filePath = uploadDir + filename + random + ".xlsx";
 
-    console.log('uploadDir '+uploadDir);
-    console.log('filePath '+filePath);
-    fs.writeFile(filePath, result, 'binary',function(err){
-        if(err){
-            console.log(err);
-        }
-    });
+    // var random = Math.floor(Math.random()*10000+0);
+
+    // var uploadDir = 'public/upload/';
+    // var filePath = uploadDir + filename + random + ".xlsx";
+
+    // console.log('uploadDir '+uploadDir);
+    // console.log('filePath '+filePath);
+    // fs.writeFile(filePath, result, 'binary',function(err){
+    //     if(err){
+    //         console.log(err);
+    //     }
+    // });
+
+    // res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    // res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+    // res.end(result, 'binary');
+
+
+
+
 
 	// SpreadsheetReader.read('a.xlsx', function (err, workbook) {
 	// 	// Iterate on sheets
@@ -88,14 +97,21 @@ router.post('/', function(req, res) {
 	// 	});
 	// });
 
+
+
+
+
 	// var xlsxPath = path.resolve(__dirname, '../public/avatar/0.0.5849197695497423.xlsx');
 	// console.log(xlsxPath);
 
-	// var obj = xlsx.parse('a.xlsx');
-	// console.log(JSON.stringify(obj));
+
+	var obj = xlsx.parse('a.xlsx');
+	console.log(JSON.stringify(obj));
 
 	// var file = xlsx.build(obj);
 	// fs.writeFileSync('b.xlsx', file, 'binary');
+
+
 
 
 
@@ -147,8 +163,8 @@ router.post('/', function(req, res) {
 
 
 
-	res.locals.success = '上传成功';
-	res.render('excel', { title: TITLE ,content:'上传文件'});
+	// res.locals.success = '上传成功';
+	// res.render('excel', { title: TITLE ,content:'上传文件'});
 });
 
 module.exports = router;
