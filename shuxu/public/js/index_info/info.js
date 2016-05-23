3ec0500e01e2d625732b30a7728e1f34ed93bc02
pankai <loveyoubaby5120@sina.com>
2016-05-23 12:28:58 +0800

    log

--- shuxu/public/js/index_info/info.js
+++ shuxu/public/js/index_info/info.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router'
-import Left from './info/left.js'
-import Right from './info/right.js'
 
 export default class Info extends React.Component {
   render() {
 
     return (
         <div className="info">
-	        <Left></Left>
-	        <Right></Right>
         </div>
     )
   }
