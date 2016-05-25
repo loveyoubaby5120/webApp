e1d0203308f1716a8d0864d06cd0c45f8efbf72f
pankai <loveyoubaby5120@sina.com>
2016-05-25 14:49:05 +0800

    log

--- shuxu/app/contorllers/gzh.server.controller.js
+++ shuxu/app/contorllers/gzh.server.controller.js
@@ -7,9 +7,48 @@ var connection = mysqldb();
 c = wrapper(connection);
 // p = wrapper(pool);
 
+// Date.prototype.format = function (format) {
+//     var date = {
+//         "M+": this.getMonth() + 1,
+//         "d+": this.getDate(),
+//         "h+": this.getHours(),
+//         "m+": this.getMinutes(),
+//         "s+": this.getSeconds(),
+//         "q+": Math.floor((this.getMonth() + 3) / 3),
+//         "S+": this.getMilliseconds()
+//     };
+//     if (/(y+)/i.test(format)) {
+//         format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+//     }
+//     for (var k in date) {
+//         if (new RegExp("(" + k + ")").test(format)) {
+//             format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+//         }
+//     }
+//     return format;
+// };
+
+// Date.prototype.timestamp = function(v){
+//     if (/^(-)?\d{1,10}$/.test(v)) {
+//         v = v * 1000;
+//     } else if (/^(-)?\d{1,13}$/.test(v)) {
+//         v = v * 1;
+//     } else {
+//         console.log("时间戳格式不正确");
+//         return;
+//     }
+//     var dateObj = new Date(v);
+//     if (dateObj.format('yyyy') == "NaN") { /*alert("时间戳格式不正确");*/return; }
+//     return dateObj;
+// }
+
+
+
+// console.log(new Date().timestamp(1464061279).format('yyyy-MM-dd/q'));
+
 module.exports = {
 	list: function *(next){
-		var sql = 'select * from article_profile limit 2';
+		var sql = 'select *,from_unixtime(pub_time,"%Y-%m-%d") as dateTime from article_profile limit 10';
 
 
 		var rows = yield c.query(sql);
