3b683d1f35153db7bfeb08554d54de13eb2614b5
pankai <loveyoubaby5120@sina.com>
2016-06-14 22:37:45 +0800

    log

--- spider/routes/bd.js
+++ spider/routes/bd.js
@@ -14,74 +14,45 @@ router.get('/', function(req, res, next) {
 
 	console.log('start spider');
 
-	var options = {
-	    uri: 'http://www.gsdata.cn/newRank/getwxranks',
-	    qs: {
-	    	gid:'205',
-			date:'2016-06-13',
-			page:'1',
-			type:'day',
-			cp:'all',
-			t:'0.9095617456105527',
-			action:'',
-	    },
-	    headers: {
-	    	'Host': 'www.gsdata.cn',
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:37.0) Gecko/20100101 Firefox/37.0',
-			'Accept': 'application/json, text/javascript, */*; q=0.01',
-			'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-			'Accept-Encoding': 'gzip, deflate, sdch',
-			'X-Requested-With': 'XMLHttpRequest',
-			'Referer': 'http://www.gsdata.cn/rank/detail',
-			'Cookie': 'PHPSESSID=sk9he9uvqb7i7vfd08no2q2ho2; bdshare_firstime=1465889026970; Hm_lvt_293b2731d4897253b117bb45d9bb7023=1465887653; Hm_lpvt_293b2731d4897253b117bb45d9bb7023=1465889858',
-			'Connection': 'keep-alive'
-	    },
-	    json: true // Automatically parses the JSON string in the response 
-	};
-
-
-	var reOptions = {
-		url: 'http://www.gsdata.cn/newRank/getwxranks',
-	    qs: {
-	    	gid:'205',
-			date:'2016-06-13',
-			page:'1',
-			type:'day',
-			cp:'all',
-			t:'0.9095617456105527',
-			action:'',
-	    },
-		headers: {
-	    	'Host': 'www.gsdata.cn',
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:37.0) Gecko/20100101 Firefox/37.0',
-			'Accept': 'application/json, text/javascript, */*; q=0.01',
-			'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-			'Accept-Encoding': 'gzip, deflate, sdch',
-			'X-Requested-With': 'XMLHttpRequest',
-			'Referer': 'http://www.gsdata.cn/rank/detail',
-			'Cookie': 'PHPSESSID=sk9he9uvqb7i7vfd08no2q2ho2; bdshare_firstime=1465889026970; Hm_lvt_293b2731d4897253b117bb45d9bb7023=1465887653; Hm_lpvt_293b2731d4897253b117bb45d9bb7023=1465889858',
-			'Connection': 'keep-alive'
-	    },
-	    json: true
-	}
+	var date = '2016-06-13';
 
 	var Json = [];
 	var count = 0;
 	var errorNum = 0;
 	var success = 0;
 	for(var z=0; z<365; z++){
-		var d = new Date(new Date(options.qs.date).getTime()-1000*60*60*24).toISOString().slice(0,10);
-		options.qs.date = d;
+		var d = new Date(new Date(date).getTime()-1000*60*60*24).toISOString().slice(0,10);
+		date = d;
 
-		reOptions.qs.date = d;
 		for(var i =1 ; i<6; i++){
-			options.qs.page = i;
-			options.qs.t = Math.random();
 
-			reOptions.qs.page = i;
-			reOptions.qs.t = Math.random();
+			var options = {
+			    uri: 'http://www.gsdata.cn/newRank/getwxranks',
+			    qs: {
+			    	gid:'205',
+					date:d,
+					page:i,
+					type:'day',
+					cp:'all',
+					t:Math.random(),
+					action:'',
+			    },
+			    headers: {
+			    	'Host': 'www.gsdata.cn',
+					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:37.0) Gecko/20100101 Firefox/37.0',
+					'Accept': 'application/json, text/javascript, */*; q=0.01',
+					'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+					'Accept-Encoding': 'gzip, deflate, sdch',
+					'X-Requested-With': 'XMLHttpRequest',
+					'Referer': 'http://www.gsdata.cn/rank/detail',
+					'Cookie': 'PHPSESSID=sk9he9uvqb7i7vfd08no2q2ho2; bdshare_firstime=1465889026970; Hm_lvt_293b2731d4897253b117bb45d9bb7023=1465887653; Hm_lpvt_293b2731d4897253b117bb45d9bb7023=1465889858',
+					'Connection': 'keep-alive'
+			    },
+			    json: true // Automatically parses the JSON string in the response 
+			};
+			
 
-			Json.push(reOptions);
+			Json.push(options);
 
 			// (function(num,op){
 			// 	request(op,function(error, response,body){
@@ -114,7 +85,7 @@ router.get('/', function(req, res, next) {
 			// 		}
 
 			// 	});
-			// })(count,reOptions);
+			// })(count,options);
 			
 			
 
@@ -144,29 +115,46 @@ router.get('/', function(req, res, next) {
 			// 		console.log('一共错误' + errorNum + '条');
 			// 		console.log('第' + num + '条');
 			// 	});
-			// })(count,options)
+			// })(count,options);
 			
 
 			count++;
 		}
 	}
 
+	
+	httpRequest(Json,0,100);
+	
 
-	async.forEachLimit(Json, 10, function(item, callback){
+	console.log(count);
+	console.log('end spider');
+
+
+	// WeChat.remove(function(err){
+	//     if(!err){
+	//         console.log('删除数据');
+	//     }
+	// });
+
+});
+
+
+
+function httpRequest(Json,str,end){
+	var arr = Json.slice(str,end);
+	async.forEachLimit(arr, 100, function(item, callback){
 		request(item,function(error, response,body){
 			if(!error && response.statusCode ==200){
-				success++;
-				console.log('成功获取' + success + '条');
 				if(body){
 					for(var j = 0; j < body.data.rows.length; j++){
-						// var wechat = new WeChat(body.data.rows[j]);
-						// wechat.save(function(err){
-						// 	if(err){
-						// 		return next(err);
-						// 	}
+						var wechat = new WeChat(body.data.rows[j]);
+						wechat.save(function(err){
+							if(err){
+								return next(err);
+							}
 							
-						// 	return j;
-						// });
+							return j;
+						});
 
 					}
 				}
@@ -175,31 +163,24 @@ router.get('/', function(req, res, next) {
 				}
 			}
 			else{
-				errorNum++;
 				console.log(error);
-				console.log('一共错误' + errorNum + '条');
-				console.log('第' + num + '条');
-
+				console.log(response);
+			}
+			if(arr[arr.length-1]==item && end <= Json.length){
+				console.log(end);
+				httpRequest(Json,end,(end+100));
 			}
 
+		},function(err, result){
+			if(err){
+				console.log('获取链接失败');
+			}else{
+				console.log('获取链接结束');
+			}
+			
 		});
 	});
-
-	console.log(count);
-	console.log('end spider');
-
-	// var pagesize = parseInt(req.query.pagesize,10) || 10;
-	// var pagestart = parseInt(req.query.pagestart,10) || 10;
-
-	// WeChat
-	// .find()
-	// .exec(function(err, docs){
-	// 	if(err){
-	// 		return next(err);
-	// 	}
-	// 	return res.json(docs.length);
-	// });
-});
+}
 
 
 module.exports = router;
