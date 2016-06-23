2b9ec28a59d00192b118427b4a8a534ed1771020
pankai <loveyoubaby5120@sina.com>
2016-06-23 11:53:58 +0800

    log

--- spider/config/env/development.js
+++ spider/config/env/development.js
@@ -1,4 +1,12 @@
 module.exports = {
 	port: 3000,
-	mongodb: 'mongodb://localhost/spider'
+	mongodb: 'mongodb://localhost/spider',
+	mysql:{
+		host: '120.27.26.133',
+		port: 3306,
+		user: 'root',
+		password: 'admin123',
+		database: 'weixin',
+		connectionLimit: 50
+	}
 }
